7401b90c142cac954d4f749d91f236c95644ebf2
Henry Arnold <devharnold@gmail.com>
2024-03-24 13:44:58 +0300

    make file again

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -13,20 +13,22 @@ def minOperations(n):
     Returns:
     int: The fewest number of ops needed
     """
-    if not isinstance(n, int):
+    if not isinstance(n, int) or n < 1:
         return 0
     #set op_count => `operations_count` to 0
     op_count = 0
     clipboard = 0
     done = 1
 
-    for done in range(1, n + 1):
+    while done < n:
         if clipboard == 0:
             clipboard = done
             op_count += 2
+            done += clipboard 
         elif n - done > 0 and (n - done) % done == 0:
             clipboard = done
             op_count += 2
+            done += clipboard 
         elif clipboard > 0:
             op_count += 1
     return op_count
