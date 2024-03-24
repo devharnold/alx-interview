90ea0b8f75fae8d89dfb6b0dd71a5d4a9c8dbd73
Henry Arnold <devharnold@gmail.com>
2024-03-24 13:55:33 +0300

    change code

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-import os
-"""
-minOperations module
-"""
+'''
+Minimum Operations coding interview
+'''
 
 def minOperations(n):
-    """function that computes the fewest number of ops
+    '''
+    function that computes the fewest number of ops
     to result in exactly n H characters.
     
     Args:
     n (int): The desired number of H characters.
     Returns:
     int: The fewest number of ops needed
-    """
+    '''
     if not isinstance(n, int):
         return 0
     #set op_count => `operations_count` to 0
@@ -21,16 +21,19 @@ def minOperations(n):
     done = 1
 
     while done < n:
+        #if clipboard is empty, copy all and paste
         if clipboard == 0:
             clipboard = done
-            done += clipboard
-            ops_count += 2 
+            done += clipboard #Increment done by clipboard size
+            ops_count += 2 #Increment operations count by 2 (copy and paste)
+        #If (n - done) is divisible by done, copy all and paste
         elif n - done > 0 and (n - done) % done == 0:
             clipboard = done
-            done += clipboard 
-            ops_count += 2
+            done += clipboard # Increment done by clipboard size
+            ops_count += 2 # Increment operations count by 2 (copy and paste)
+        #If clipboard has content, paste
         elif clipboard > 0:
-            done += clipboard
-            ops_count += 1
+            done += clipboard #Increment done by clipboard size
+            ops_count += 1 # Increment operations count by 1(paste)
     return ops_count
 
